14cbb918e74cddaccbe0301cdf994c703d2f8f3a
Sobuj <sobujkumar424@gmail.com>
2022-03-15 20:44:46 +0600

    added shotcut

--- js/countris.js
+++ js/countris.js
@@ -12,12 +12,36 @@ const displayCountries = countries => {
 
 
 }
-const getCountryHTML = country => {
+// option 1
+/* const getCountryHTML = (country) => {
+    const {name, flags} = country;
+    return `
+     <div class="countris">
+        <h2>${name.common}</h2>
+        <img src="${flags.png}">
+     </div>
+    `
+} */
+// option 2
+const getCountryHTML = ({name, flags, area, region}) => {
+    return `
+     <div class="countris">
+        <h2>${name.common}</h2>
+        <p>Area: ${area}</p>
+        <p>Continet: ${region}</p>
+        <img src="${flags.png}">
+     </div>
+    `
+}
+// original
+/*
+  const getCountryHTML = country => {
     return `
      <div class="countris">
         <h2>${country.name.common}</h2>
         <img src="${country.flags.png}">
      </div>
     `
-}
+} 
+*/
 loadCountries();
